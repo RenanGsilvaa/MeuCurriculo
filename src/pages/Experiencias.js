33241912bcdbfbb605bd8c937cc34cf4cf7948d0
RenanGSilvaa <profissionalrenann@outlook.com>
2023-04-19 21:56:27 -0300

    Criando o Botão e Finalizando(eu acho que finalizando)

--- src/pages/Experiencias.js
+++ src/pages/Experiencias.js
@@ -1,13 +1,15 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Botao from '../components/Botao';
 
 const Experiencias = ({navigation}) => {
     return (
-      <View style={styles.container}>
+      <View style={styles.container}>        
         <Text>Experiencias</Text>
         <View>
-        <Text>Trabalhos</Text>
-            <Text>Auxiliar de Entrada de Nf - Estágio - Supermercado RedePas Jordão</Text>
-            <Text>Auxiliar de escritório - Jovem Aprendiz - De Heus</Text>
+            <Text>Trabalhos</Text>
+              <Text>Auxiliar de Entrada de Nf - Estágio - Supermercado RedePas Jordão</Text>
+              <Text>Auxiliar de escritório - Jovem Aprendiz - De Heus</Text>
+              <Botao titulo={"Eu sou um botão, se clicar em mim vai para sobre"} acao={() => navigation.navigate("Sobre")}/>
         </View>
             <Button title="Voltar" onPress={ () => navigation.goBack()} />
       </View>
@@ -20,6 +22,10 @@ const Experiencias = ({navigation}) => {
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: '',
+    },
+
+    foto: {
+      borderRadius: '5px',
     },
   });
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 const Home = ({ navigation }) => {
     return (
       <View style={styles.container}>
+        <View>
+          <Image source={require('../images/FotoMeuCurriculo.jpg')} style={{ width: 175, height: 175, borderRadius: 100}}/>
+        </View>
         <Text>Olá, meu nome é Renan Gabriel da Silva. Esse é o meu primeiro segundo na verdade.</Text>
         <Text>O objetivo desse aplicativo é se comportar como se fosse um currículo, o meu no caso.</Text>
         <Text>Espero que goste.</Text> 
@@ -11,7 +14,7 @@ const Home = ({ navigation }) => {
         <Button title='Experiências' onPress={() => navigation.navigate('Experiencias')} />
         <Button title='Pontos Fortes' onPress={() => navigation.navigate('PontosFortes')} />
         <Button title='Pontos Fracos' onPress={() => navigation.navigate('PontosFracos')} />
-        <Button title='Gostos' onPress={() => navigation.navigate('Gostos')}/>
+        <Button title='Gostos' onPress={() => navigation.navigate('Gostos')} />
       </View>
     );
   }
@@ -22,7 +25,9 @@ const Home = ({ navigation }) => {
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: '',
-    },
+      padding: 30,
+      margin: 25,
+    }
   });
 
 export  default Home;
